9f1f78b62bd5dd3d5413134a8ae98f6de55b2b00
reitnorF <adipurnama@s.itb.ac.id>
2020-04-09 14:40:18 +0700

    Smooth scroll

--- pub/engine.js
+++ pub/engine.js
@@ -35,6 +35,7 @@ function titleCaser(item) {
 	return combined
 }
 
+//Copy "str" string to clipboard
 function copyToClipboard(str){
 	var el = document.createElement('textarea');
 	el.value = str;
@@ -46,24 +47,21 @@ function copyToClipboard(str){
 	document.body.removeChild(el);
 }
 
-
+//DragDrop to Upload
 var dropArea = document.querySelector("#dropzone");
 var everything = document.querySelector("body");
 dropArea.addEventListener("dragenter",preventDefaults,false);
 dropArea.addEventListener("dragover",preventDefaults,false);
 dropArea.addEventListener("dragleave",preventDefaults,false);
 dropArea.addEventListener("drop",preventDefaults,false);
-
 dropArea.addEventListener("dragenter",highlight,false);
 dropArea.addEventListener("dragover",highlight,false);
 dropArea.addEventListener("dragleave",unhighlight,false);
 dropArea.addEventListener("drop",unhighlight,false);
-
 everything.addEventListener("dragenter",highlight,false);
 everything.addEventListener("dragover",highlight,false);
 everything.addEventListener("dragleave",unhighlight,false);
 everything.addEventListener("drop",unhighlight,false);
-
 dropArea.addEventListener("drop",handleDrop,false);
 
 function preventDefaults(e){
@@ -92,7 +90,6 @@ function uploadFile(file){
 	var fullURL = window.location.href;
   fullURL = fullURL.split("/")
   var n = fullURL[fullURL.length-2]+"."+fullURL[fullURL.length-1]
-
 	console.log(url);
 	var formData = new FormData()
 	formData.append('n',n)
@@ -115,10 +112,21 @@ function uploadFile(file){
 }
 
 
+var masterTitle = document.title;
+var nodeSourceCode = "";
+
+//Asynchronous Save Changes
+//Called once every 3 minutes
 function saveChanges(){
+	console.log("Past Node Source Code :" + nodeSourceCode);
 	var n = document.querySelector("form[name='nodeeditor'] input[name='n']").value
 	var basetime = document.querySelector("form[name='nodeeditor'] input[name='basetime']").value
 	var textContent = document.querySelector("#text").value
+	if (textContent == nodeSourceCode) {
+		console.log("No changes, abort autosave..");
+		return 0;
+	}
+
 	var postbody2 = new URLSearchParams()
 	postbody2.append("action","edit")
 	postbody2.append("n",n)
@@ -139,20 +147,21 @@ function saveChanges(){
 			var oldTitle = document.title;
 			document.title = "Saved...";
 			setTimeout(function(){
-				document.title = oldTitle;
 				var d = new Date()
+				var h = d.getHours()
 				var m = d.getMinutes()
-				oldTitle = m + " " + oldTitle
+				oldTitle = "["+h+":"+m + "] " + masterTitle
 				document.title = oldTitle
 
 			}, 1000);
 			e.text().then(function(r){
-				console.log(r)
+				//console.log(r)
 				var retrievedHTML = r 
 				var newDateRegex = /name='basetime' value='(.*)'/g;
 				var match = newDateRegex.exec(r)
 				var timeCode = match[1]
 				document.querySelector("form[name='nodeeditor'] input[name='basetime']").value = timeCode
+				nodeSourceCode = textContent;
 			})		
 	})
 	.catch((e)=>{
@@ -161,6 +170,9 @@ function saveChanges(){
 		})
 }
 
+
+//Synchronous Save Changes
+//Called once, just a moment before exiting the application.
 function syncSaveChanges(){
 	document.title = "Saving..";
 	var n = document.querySelector("form[name='nodeeditor'] input[name='n']").value
@@ -184,50 +196,147 @@ function syncSaveChanges(){
 }
 
 
-//Shortcut Functionality
-//Ctrl + . = New node
-//Only work for main namespace :(
-document.onkeyup = function(e) {
-  //console.log(e.which);
-  if (e.ctrlKey && e.which == 190) {
-	var a = prompt("Node name");
-	if (a != null){
-		var split_a = a.split(" ");
-		console.log(split_a);
-		var i = 0;
-		for (var i=0; i <split_a.length; i++){
-			split_a[i] = titleCaser(split_a[i])
+//Core Save Functionality
+//If currently on Edit Mode, enable autosave each 3 minutes and always saving before unload
+var thisPageURL = window.location.href;
+if (thisPageURL.includes("?action=edit")) {
+	nodeSourceCode = document.querySelector("#text").value;
+	var intervalID = setInterval(saveChanges,180000)
+	window.onbeforeunload = function(){
+		syncSaveChanges();
+		return null;
+	}	
+}
+
+
+
+function cursorScroll(x){
+	var text = document.getElementById("text");
+	var cursorPos = text.selectionStart;
+	var textVal = text.value;
+	var counter = 0;
+
+	//Code 1 means go down 5 lines
+	if(x == 1) {
+		for (var i=cursorPos ; i <textVal.length ;i++) {
+			if(textVal.charAt(i) == '\n'){
+				counter = counter + 1;
+			}
+			if(counter == 6) {
+				break;
+			}
 		}
-		a = ""
-		for (var i=0; i <split_a.length; i++){
-			a = a + split_a[i]
+		if(i != textVal.length) {
+			i = i +1;
 		}
-		baseURL = getBaseURL()
-		var URL = baseURL + a + "?action=edit";
-		window.location = baseURL + a + "?action=edit";
 	}
+	//Code 2 means go up 5 lines
+	else if (x == 2) {
+		for (var i=cursorPos ; i >= 0 ;i--) {
+			if(textVal.charAt(i) == '\n'){
+				counter = counter + 1;
+			}
+			if(counter == 6) {
+				break;
+			}
+		}
+		if(i != 0) {
+			i = i -1;
+		}
+	}
+
+
+	text.selectionStart = i;
+  text.selectionEnd = i;
+ 
+}
+
+
+document.onkeydown = function(e){
+
+	if (e.which == 34) {
+  	e.preventDefault();
+  	cursorScroll(1);
   }
+  else if (e.which == 33) {
+  	e.preventDefault();
+  	cursorScroll(2);
+  }
+
+}
+
+
+//Shortcut Functionality
+document.onkeyup = function(e) {
+  
+  //Ctrl+. : Create New Node
+  if (e.ctrlKey && e.which == 190) {
+  	var baseURL = getBaseURL()
+  	var splitBaseURL = baseURL.split("/")
+  	var filtered = splitBaseURL.filter(function(el){
+  		return el != ""
+  	})
+  	
+  	if (filtered.length == 3){
+  		baseURL += "Main/"
+  	}
+
+		var a = prompt("Node name");
+		if (a != null){
+			var split_a = a.split(" ");
+			console.log(split_a);
+			var i = 0;
+			for (var i=0; i <split_a.length; i++){
+				split_a[i] = titleCaser(split_a[i])
+			}
+			a = ""
+			for (var i=0; i <split_a.length; i++){
+				a = a + split_a[i]
+			}
+			var URL = baseURL + a + "?action=edit";
+			console.log(URL);
+			window.location = URL;
+			//window.location = baseURL + a + "?action=edit";
+		}
+  }
+
+  else if (e.ctrlKey && e.which == 191){
+  	var baseURL = getBaseURL()
+  	var splitBaseURL = baseURL.split("/")
+  	var filtered = splitBaseURL.filter(function(el){
+  		return el != ""
+  	})
+  	
+  	if (filtered.length == 3){
+  		baseURL += "Main/"
+  	}
+
+		var a = prompt("Node name");
+		if (a != null){
+			var split_a = a.split(" ");
+			console.log(split_a);
+			var i = 0;
+			for (var i=0; i <split_a.length; i++){
+				split_a[i] = titleCaser(split_a[i])
+			}
+			a = ""
+			for (var i=0; i <split_a.length; i++){
+				a = a + split_a[i]
+			}
+			var URL = baseURL + a;
+			console.log(URL);
+			window.location = URL;
+			//window.location = baseURL + a + "?action=edit";
+		}
+
+  }
+
+  //Ctrl+, : Edit Current Node
   else if (e.ctrlKey && e.which == 188) {
 	window.location = window.location.href+"?action=edit";
   }
-  else if (e.ctrlKey && e.which == 191) {
-	var a = prompt("Node name");
-	if (a != null){
-		var split_a = a.split(" ");
-		console.log(split_a);
-		var i = 0;
-		for (var i=0; i <split_a.length; i++){
-			split_a[i] = titleCaser(split_a[i])
-		}
-		a = ""
-		for (var i=0; i <split_a.length; i++){
-			a = a + split_a[i]
-		}
-		baseURL = getBaseURL()
-		var URL = baseURL + a + "?action=edit";
-		window.location = baseURL + a;
-	}
-  }
+
+  //Ctrl+; : Copy Current Node
   else if (e.ctrlKey && e.which == 59){
   	var fullURL = window.location.href;
   	fullURL = fullURL.split("/")
@@ -241,9 +350,14 @@ document.onkeyup = function(e) {
   		document.title = oldTitle;
   	}, 1000);
   }
+
+  //Ctrl+' : Save Node (Deprecated)
   else if (e.ctrlKey && e.which == 222){
   	saveChanges()
   }
+  
+
+
 };
 
 //"Class" Functionality
@@ -254,30 +368,33 @@ target = target.replace("%3C/p%3E","")
 target = target.replace("%3Cp%3E","")
 target = target.replace(new RegExp("%20","g"),"");
 console.log(target);
-
-
 targetnode.href = target
 
 
+//Autogrowing Node Editor Textarea
 var textarea = window.document.querySelector("textarea");
 if(textarea){	
 	textarea.onkeyup = function(e){
+
+		if (textarea.clientHeight < textarea.scrollHeight){
+			textarea.style.height = textarea.scrollHeight +"px";
+		}
 		if(textarea.scrollTop != 0){
 			textarea.style.height = textarea.scrollHeight + "px";
+			window.scrollBy(0,205);
 			console.log("Did something! " + textarea.scrollTop)
 		}
 	
 		if(e.which == 34){
+			
 			window.scrollBy(0,125);	
 		}
 		else if (e.which == 33){
+			
 			window.scrollBy(0,-125);
 		}
 	}
 }
-
-
-
 
 window.addEventListener("load",function() {
 	textarea = window.document.querySelector("textarea");
@@ -287,12 +404,9 @@ window.addEventListener("load",function() {
 },false);
 
 
-window.onbeforeunload = function(){
-	syncSaveChanges();
-	return null;
-}
 
-var intervalID = setInterval(saveChanges,300000)
+
+
 
 //Img Server Functionality
 var target1 = document.getElementById("imgstrg").innerHTML
@@ -400,14 +514,6 @@ for (var i=0; i<wikilinks.length; i++){
 		formData.append('nodekey',nodekey);
 		xhttp.send(formData)
 
-
-		/*
-		console.log(this.href)
-		const url = this.href
-		fetch(url)
-		.then(function(data){console.log(data.text())})
-		.catch(function(error){console.log(error)})
-		*/
 
 	}
 }
